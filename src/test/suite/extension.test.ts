01d80f4b3f0da74776252981fdc2dd248e32bfe6
Matthias Küch <mail@matthias-kuech.de>
2019-11-23 15:57:34 +0100

    Comment faulty test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -15,7 +15,21 @@ suite('Extension Test Suite', () => {
 		assert.equal(runscript.getCommandFromFile(pathToTestScripts + "example_test_script.js", "utf-8"), "uname -a");
 	});
 
-	test('Run Script in acutal VSCode', () => {
+	// test('Run Script in acutal VSCode', () => {
 
-	});
+	// 	var setting: vscode.Uri = vscode.Uri.parse(pathToTestScripts+"example_unit_test.sh");
+	// 	vscode.workspace.openTextDocument(setting).then((a: vscode.TextDocument) => {
+	// 		vscode.window.showTextDocument(a, 1, false).then(e => {
+	// 			// e.edit(edit => {
+	// 			// 	edit.insert(new vscode.Position(0, 0), "Your advertisement here");
+	// 			// });
+	// 			vscode.commands.executeCommand('extension.runscript');
+	// 			console.log("showing document");
+	// 		});
+	// 	}, (error: any) => {
+	// 		console.error(error);
+	// 		debugger;
+	// 	});
+
+	// });
 });
